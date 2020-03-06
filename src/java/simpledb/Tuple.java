6466dae14ad898a9b06f45e80fbd1b09ec73fb99
Facwl <2864235156@qq.com>
2020-03-06 22:25:09 +0800

    Tuple Finish Ver1  //不知为何List形式过不了编译

--- src/java/simpledb/Tuple.java
+++ src/java/simpledb/Tuple.java
@@ -1,8 +1,7 @@
 package simpledb;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * Tuple maintains information about the contents of a tuple. Tuples have a
@@ -13,6 +12,7 @@ public class Tuple implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+
     /**
      * Create a new tuple with the specified schema (type).
      *
@@ -20,26 +20,35 @@ public class Tuple implements Serializable {
      *            the schema of this tuple. It must be a valid TupleDesc
      *            instance with at least one field.
      */
+
+    private TupleDesc tupleDesc; //表头
+    private Field[] fields; //数据
+
     public Tuple(TupleDesc td) {
         // some code goes here
-    }
+        this.tupleDesc=td;
+        fields=new Field[td.numFields()];
+    }//未完
 
     /**
      * @return The TupleDesc representing the schema of this tuple.
      */
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
-    }
+        return this.tupleDesc;
+    }//get
 
     /**
      * @return The RecordId representing the location of this tuple on disk. May
      *         be null.
      */
+    RecordId recordId;
+
     public RecordId getRecordId() {
         // some code goes here
-        return null;
-    }
+
+        return recordId;
+    }//
 
     /**
      * Set the RecordId information for this tuple.
@@ -48,7 +57,7 @@ public class Tuple implements Serializable {
      *            the new RecordId for this tuple.
      */
     public void setRecordId(RecordId rid) {
-        // some code goes here
+        recordId=rid;
     }
 
     /**
@@ -61,6 +70,9 @@ public class Tuple implements Serializable {
      */
     public void setField(int i, Field f) {
         // some code goes here
+        if(i<0||i>=tupleDesc.numFields())
+            throw new IllegalArgumentException("Field索引非法");
+        fields[i]=f;
     }
 
     /**
@@ -71,20 +83,33 @@ public class Tuple implements Serializable {
      */
     public Field getField(int i) {
         // some code goes here
-        return null;
+        if(i<0||i>=tupleDesc.numFields())
+            throw new IllegalArgumentException("Field索引非法");
+        if(fields[i]==null)
+            return null;
+        else
+            return fields[i];
     }
 
     /**
      * Returns the contents of this Tuple as a string. Note that to pass the
      * system tests, the format needs to be as follows:
      *
-     * column1\tcolumn2\tcolumn3\t...\tcolumnN
+     * column1\tcolumn2\tcolumn3\t...\tcolumnN\n
      *
-     * where \t is any whitespace (except a newline)
+     * where \t is any whitespace, except newline, and \n is a newline
      */
     public String toString() {
         // some code goes here
-        throw new UnsupportedOperationException("Implement this");
+        //下面这行是干嘛的
+        // throw new UnsupportedOperationException("Implement this");
+        StringBuffer tmpstr=new StringBuffer();
+        for(int j=0;j<tupleDesc.numFields()-1;++j)
+        {
+            tmpstr.append(fields[j].toString()+'\t');
+        }
+        tmpstr.append(fields[tupleDesc.numFields()-1].toString()+'\n');
+        return tmpstr.toString();
     }
 
     /**
@@ -94,14 +119,34 @@ public class Tuple implements Serializable {
     public Iterator<Field> fields()
     {
         // some code goes here
-        return null;
+        return new ItrFie();
     }
-
+    private class ItrFie implements Iterator<Field>
+    {
+        private int nump=0;
+        public boolean hasNext()
+        {
+            if(nump<fields.length)
+                return true;
+            else
+                return false;
+        }
+        public Field next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();//？
+            }
+            return fields[nump++];
+        }
+    }
     /**
      * reset the TupleDesc of this tuple (only affecting the TupleDesc)
      * */
     public void resetTupleDesc(TupleDesc td)
     {
         // some code goes here
+        this.tupleDesc=td;
+        //this.fields.clear();
+        //this.fields=new ArrayList<>(td.numFields());
+        fields=new Field[td.numFields()];
     }
 }
