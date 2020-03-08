4fa5a9b9fb36216564feb4847d06d77725f05679
Facwl <2864235156@qq.com>
2020-03-08 14:33:03 +0800

    #HeapPageId finish.

--- src/java/simpledb/HeapPageId.java
+++ src/java/simpledb/HeapPageId.java
@@ -10,14 +10,18 @@ public class HeapPageId implements PageId {
      * @param tableId The table that is being referenced
      * @param pgNo The page number in that table.
      */
+    private int tableId;
+    private int pgNo;
     public HeapPageId(int tableId, int pgNo) {
         // some code goes here
+        this.tableId=tableId;
+        this.pgNo=pgNo;
     }
 
     /** @return the table associated with this PageId */
     public int getTableId() {
         // some code goes here
-        return 0;
+        return tableId;
     }
 
     /**
@@ -26,7 +30,7 @@ public class HeapPageId implements PageId {
      */
     public int getPageNumber() {
         // some code goes here
-        return 0;
+        return pgNo;
     }
 
     /**
@@ -37,7 +41,9 @@ public class HeapPageId implements PageId {
      */
     public int hashCode() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+       // throw new UnsupportedOperationException("implement this");
+
+        return (pgNo+tableId);
     }
 
     /**
@@ -49,6 +55,12 @@ public class HeapPageId implements PageId {
      */
     public boolean equals(Object o) {
         // some code goes here
+        if( o instanceof PageId)
+        {
+            PageId tmpId=(PageId) o;
+            if(tmpId.getTableId()==this.tableId && tmpId.getPageNumber()==this.pgNo)
+                return true;
+        }
         return false;
     }
 
