07d0d4f376c7e1f3ce36b975a24d2aa0ba1cb854
Facwl <2864235156@qq.com>
2020-03-29 23:26:01 +0800

    # Insert Finish

--- src/java/simpledb/Insert.java
+++ src/java/simpledb/Insert.java
@@ -1,5 +1,9 @@
 package simpledb;
 
+import com.sun.deploy.security.SelectableSecurityManager;
+
+import java.io.IOException;
+
 /**
  * Inserts tuples read from the child operator into the tableId specified in the
  * constructor
@@ -21,26 +25,49 @@ public class Insert extends Operator {
      *             if TupleDesc of child differs from table into which we are to
      *             insert.
      */
+    private TransactionId tid;
+    private OpIterator thechild;
+    private int tableId;
+    private int called;
+    private TupleDesc theTd;
+
     public Insert(TransactionId t, OpIterator child, int tableId)
             throws DbException {
         // some code goes here
+        this.tid =t;
+        this.thechild=child;
+        this.tableId=tableId;
+        this.called=0;
+        this.theTd=new TupleDesc(new Type[]{Type.INT_TYPE});
+
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        //看定义！
+        //return this.thechild.getTupleDesc();
+        return this.theTd;
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        super.open();
+        this.thechild.open();
     }
 
     public void close() {
         // some code goes here
+        super.close();
+        this.thechild.close();
+        this.called=0;
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+
+        this.rewind();
+        this.called=0;
+
     }
 
     /**
@@ -56,19 +83,42 @@ public class Insert extends Operator {
      * @see Database#getBufferPool
      * @see BufferPool#insertTuple
      */
+    // 重要！！！ 不是一个tp 是一整串！
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        if(this.called==1)
+            return null;
+        called=1;
+        int cnt=0;
+        while (this.thechild.hasNext())
+        {
+            Tuple tmpTp=thechild.next();
+            try {
+                Database.getBufferPool().insertTuple(this.tid, this.tableId, tmpTp);
+                cnt++;
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }       // 记得加Try!!!!!!!不要试图改源码
+        }
+
+        Tuple rtTp=new Tuple(this.theTd);
+        //类型转换！！！！！！！！！！！！！
+        IntField tmpf=new IntField(cnt);
+        rtTp.setField(0,tmpf);
+        return rtTp;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[]{this.thechild};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        this.thechild=children[0];
     }
 }
