beac8771210186410c1015f5641fe575b5ac7bb5
Facwl <2864235156@qq.com>
2020-03-30 08:11:21 +0800

    # Delete Finish

--- src/java/simpledb/Delete.java
+++ src/java/simpledb/Delete.java
@@ -19,25 +19,45 @@ public class Delete extends Operator {
      * @param child
      *            The child operator from which to read tuples for deletion
      */
+    private TransactionId tid;
+    private OpIterator thechild;
+    private TupleDesc theTd;
+    private int cnt;
+    private int called;
     public Delete(TransactionId t, OpIterator child) {
         // some code goes here
+        this.tid=t;
+        this.thechild=child;
+        this.theTd=new TupleDesc(new Type[]{Type.INT_TYPE});
+        this.cnt=0;
+        this.called=0;
+        this.called=0;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        return this.theTd;
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        super.open();
+        this.thechild.open();
     }
 
     public void close() {
         // some code goes here
+        super.close();
+        this.thechild.close();
+        this.cnt=0;
+        this.called=0;
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
+        this.thechild.rewind();;
+        this.cnt=0;
+        this.called=0;
     }
 
     /**
@@ -51,18 +71,39 @@ public class Delete extends Operator {
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        if(this.called==1)
+            return null;
+        called=1;
+        while (this.thechild.hasNext())
+        {
+            Tuple tmpTp=thechild.next();
+            try {
+                Database.getBufferPool().deleteTuple(this.tid,  tmpTp);
+                cnt++;
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }       // 记得加Try!!!!!!!不要试图改源码
+        }       // 吐血。 虽然没说只能call一次，但就是只能call一次
+
+        Tuple rtTp=new Tuple(this.theTd);
+        //类型转换！！！！！！！！！！！！！
+        IntField tmpf=new IntField(cnt);
+        rtTp.setField(0,tmpf);
+        return rtTp;
     }
 
     @Override
     public OpIterator[] getChildren() {
         // some code goes here
-        return null;
+        return new OpIterator[]{this.thechild};
     }
 
     @Override
     public void setChildren(OpIterator[] children) {
         // some code goes here
+        this.thechild=children[0];
     }
 
 }
